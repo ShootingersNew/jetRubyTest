d0df36787221e0465ef65b3e61dc204a33aaf90b
shootingersNew <lebedevseergey@gmail.com>
2020-12-28 15:41:33 +0300

    Add random tile generation

--- src/components/TilesGame/index.tsx
+++ src/components/TilesGame/index.tsx
@@ -1,26 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import TilesView from "../TilesView";
 import {TileType} from "../../types";
 
 const TilesGame: React.FC = () => {
-    //"smart" comp
-    const tilesArr: TileType[] = [
-        {
-            color: '#000',
-            isOpened: false,
-            isGuessed: false
-        },
-        {
-            color: '#000',
-            isOpened: false,
-            isGuessed: false
-        },
-        {
-            color: '#000',
-            isOpened: false,
-            isGuessed: false
+    const [tiles, setTiles] = useState<TileType[] | undefined>(undefined)
+    //"smart" comp for TilesGame
+    useEffect(() => {
+
+        const randomColor: () => string = () => {
+            return Math.floor(Math.random() * 16777215).toString(16)
+        };
+        const generateRandomTiles: () => TileType[] = () => {
+            //randomize color
+            const randomizePos: (a: any, b: any) => number = (a, b) => {
+                return 0.5 - Math.random();
+            }
+            // generate an array of random colors
+            const colors = new Array(8).fill(undefined).map(() => {
+                return randomColor();
+            });
+
+            //concatenate arrays and randomize the positions of their elements
+            return [...colors, ...colors].sort(randomizePos).map((color) => {
+                return {color: `#${color}`, isOpened: false, isGuessed: false}
+            })
         }
-    ];
-    return <TilesView arr={tilesArr}/>
+        setTiles(generateRandomTiles())
+    }, [])
+
+    return <TilesView arr={tiles}/>
 }
 export default TilesGame
