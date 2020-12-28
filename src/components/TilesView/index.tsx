ef351d45e2772046a19ad70d9a876cc42ad44cd2
shootingersNew <lebedevseergey@gmail.com>
2020-12-28 15:37:29 +0300

    tiles now rendering by Tile

--- src/components/TilesView/index.tsx
+++ src/components/TilesView/index.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import './style.scss'
+import Tile from "../Tile";
 import {TileType} from "../../types";
 
 type TilesViewPropTypes = {
-    arr: TileType[],
+    arr?: TileType[],
 }
 const TilesView: React.FC<TilesViewPropTypes> = ({arr}) => {
 
-    const mapTiles: () => JSX.Element[] = () => {
-        return arr.map(({color, isGuessed, isOpened},id) => {
-            return <div key={id} className="tilesView tile" style={ isOpened && isGuessed ? {backgroundColor: `${color}`} : undefined }/>
-        })
+    const mapTiles: () => JSX.Element = () => {
+        return <>
+            {
+                arr && arr.map(({color, isGuessed, isOpened}, id) => {
+                    return (
+                        <Tile
+                            key={id}
+                            className="tilesView__tile"
+                            style={isOpened && isGuessed ? {backgroundColor: `${color}`} : undefined}
+                        />
+                    )
+                })
+            }
+        </>
     }
 
     return (
