b7e341b3e73ca99df8b024ce7d741d58de974408
shootingersNew <lebedevseergey@gmail.com>
2020-12-28 14:14:24 +0300

    render GamePage to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import GamePage from "./pages/GamePage";
 import './App.css';
+import TilesGame from "./components/TilesGame";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GamePage gameName={'TILES GAME'} children={TilesGame}/>
     </div>
   );
 }
